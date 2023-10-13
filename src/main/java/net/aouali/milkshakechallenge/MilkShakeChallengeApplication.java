aea633d58c54c464d46d833e1754fa0de25496da
Donia <donia@aouali.net>
2023-10-13 10:49:49 +0200

    first commit

--- src/main/java/net/aouali/milkshakechallenge/MilkShakeChallengeApplication.java
+++ src/main/java/net/aouali/milkshakechallenge/MilkShakeChallengeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MilkShakeChallengeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MilkShakeChallengeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MilkShakeChallengeApplication.class, args);
+    }
 
 }
